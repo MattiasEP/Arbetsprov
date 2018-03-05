75f78a1447cacbfa8cf17b54989af41f5fb78bc1
Mattias Söderström <mattias.ep@gmail.com>
2018-03-05 17:38:45 +0100

    Started working on the layout

---
+++ script.js
@@ -0,0 +1,20 @@
+class App {
+    constructor() {
+        this.apiURL = 'https://swapi.co/api/people/?search='
+
+        
+
+    }
+
+    init() {
+        this.bindEvents()
+    }
+
+    bindEvents() {
+
+
+    }
+}
+
+const app = new App()
+
